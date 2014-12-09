0a1a2703d8638e4fb254536166b339c261d4b772
Crispin Boylan <crisb@mandriva.org>
2014-12-09 20:33:22 +0000

    Add tarball script

---
+++ generate_tarball.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+aid="javax.el-api"
+version=`grep Version: *spec | sed -e 's/Version:\s*\(.*\)/\1/'`
+filename="${aid}-${version}-sources.jar"
+tempdir="glassfish-el-api-${version}"
+url="http://central.maven.org/maven2/javax/el/${aid}/${version}/${aid}-${version}-sources.jar"
+
+echo $version
+
+rm -Rf ${tempdir}
+mkdir ${tempdir}
+
+pushd ${tempdir}
+  wget ${url}
+  unzip ${filename}
+  mv META-INF/LICENSE.txt .
+  rm -Rf ${filename} META-INF/
+  mkdir -p src/main/java
+  mv javax/ src/main/java
+popd
+
+tar czvf glassfish-el-api-${version}.tar.gz ${tempdir}
+
+rm -Rf ${tempdir}
